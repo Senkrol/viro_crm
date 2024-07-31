37d9f290546177a82c99b86a862571ee206d9085
Roman <senkrol.srw@gmail.com>
2024-07-31 12:45:03 +0300

    kit start

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps({
+
+});
+</script>
+<template>
+
+  <Head title="Welcome" />
+  <header>
+
+    <div v-if="$page.props.auth.user">
+      <Link :href="route('dashboard')">
+      Dashboard
+      </Link>
+    </div>
+
+    <div v-else>
+      <Link :href="route('login')">
+      Log in
+      </Link>
+
+      <Link :href="route('register')">
+      Register
+      </Link>
+      
+    </div>
+
+  </header>
+</template>
